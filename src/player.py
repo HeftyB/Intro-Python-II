7eefae412acf7f9a1b30ce05df740f38e660c6e1
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-08-05 19:23:57 -0400

    completed day 1 MVP, and a little of day 2's

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        self.health = 100
+        # self.inventory = []
+    
+    # @property
+    # def add_inventory_item(item):
+    #     self.inventory.append(item)
+    
+    # @property
+    # def get_inventory:
+    #     return self.inventory
